a7f1493613c68f6e617488e61321ab92cfc6a7bb
Amaan Abbas <amaan140904@gmail.com>
2025-02-12 02:40:08 +0530

    non static inner class and static inner class

--- Maths/bookSysteem.java
+++ Maths/bookSysteem.java
@@ -1,18 +1,47 @@
-public class bookSysteem {
-    private String name;
+import OuterClass.InnerNestedClass;
 
-    // bookSysteem() {
-    //     this.name = "amaan";
+class OuterClass {
+    static class InnerNestedClass {
+        void display() {
+            System.out.println("This is a static inner nested class");
+        }
+    }
+}
+
+class OuterClass2 {
+    class NonStaticInnerNestedClass {
+        void display() {
+            System.out.println("This is a non static inner class!");
+        }
+    }
+}
+
+
+public class bookSysteem {
+    // private String name;
+
+    // // bookSysteem() {
+    // //     this.name = "amaan";
+    // // }
+
+    // public bookSysteem setname(String name) {
+    //     this.name = name;
+    //     return this;
     // }
 
-    public bookSysteem setname(String name) {
-        this.name = name;
-        return this;
-    }
+    // public static void main(String[] args) {
+    //     bookSysteem obj = new bookSysteem();
+
+    //     System.out.println(obj.setname("Amaan").toString());
+    // }
 
     public static void main(String[] args) {
-        bookSysteem obj = new bookSysteem();
+        OuterClass.InnerNestedClass obj = new OuterClass.InnerNestedClass();
+        obj.display();
 
-        System.out.println(obj.setname("Amaan").toString());
+        OuterClass2 ob = new OuterClass2();
+        OuterClass2.NonStaticInnerNestedClass ob1 = ob.new NonStaticInnerNestedClass();
+        ob1.display();
     }
+    
 }
