146111039535e1a230286197cd3d0768471d3326
Amaan Abbas <amaan140904@gmail.com>
2025-04-17 12:20:33 +0530

    Leetcode 28

---
+++ StringMatching/FindfirstOccurance.java
@@ -0,0 +1,25 @@
+class FindfirstOccurance {
+    public static int strStr(String haystack, String needle) {
+        int needleLength = needle.length();
+        int haystackLength = haystack.length();
+
+        System.out.println(needleLength);
+        System.out.println(haystackLength);
+
+        for (int i = 0; i <= haystackLength - needleLength; i++) {
+            int j = 0;
+            while (j < needleLength && needle.charAt(j) == haystack.charAt(i + j)) {
+                j++;
+            }
+
+            if (j == needleLength) return i;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String a = "sadbutsad", b = "sad";
+        System.out.println(strStr(a, b));
+    }
+}
