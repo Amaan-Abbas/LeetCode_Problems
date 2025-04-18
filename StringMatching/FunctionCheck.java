8c9c19b0addb3b8441ac3e9b8a413d29daaded12
Amaan Abbas <amaan140904@gmail.com>
2025-04-18 17:07:42 +0530

    smth

--- StringMatching/FunctionCheck.java
+++
@@ -1,7 +0,0 @@
-public class FunctionCheck {
-    public static void main(String[] args) {
-        String str = "Syed Amaan Abbas Naqvi";
-        
-        System.out.println(str.contains("maan Abb"));
-    }
-}
