9d7ed0cac4c0a04215bce8610442a3e3395f8b5a
Amaan Abbas <amaan140904@gmail.com>
2025-04-18 17:00:21 +0530

    Leect Code 459

---
+++ StringMatching/FunctionCheck.java
@@ -0,0 +1,7 @@
+public class FunctionCheck {
+    public static void main(String[] args) {
+        String str = "Syed Amaan Abbas Naqvi";
+        
+        System.out.println(str.contains("maan Abb"));
+    }
+}
