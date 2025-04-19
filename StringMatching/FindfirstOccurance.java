2c50256bdf87a4e8471e42d82595ef894ae0ff02
Amaan Abbas <amaan140904@gmail.com>
2025-04-19 14:54:13 +0530

    LeetCode 28

--- StringMatching/FindfirstOccurance.java
+++ StringMatching/FindfirstOccurance.java
@@ -1,21 +1,24 @@
 class FindfirstOccurance {
     public static int strStr(String haystack, String needle) {
-        int needleLength = needle.length();
-        int haystackLength = haystack.length();
+        // int needleLength = needle.length();
+        // int haystackLength = haystack.length();
 
-        System.out.println(needleLength);
-        System.out.println(haystackLength);
+        // System.out.println(needleLength);
+        // System.out.println(haystackLength);
 
-        for (int i = 0; i <= haystackLength - needleLength; i++) {
-            int j = 0;
-            while (j < needleLength && needle.charAt(j) == haystack.charAt(i + j)) {
-                j++;
-            }
+        // for (int i = 0; i <= haystackLength - needleLength; i++) {
+        //     int j = 0;
+        //     while (j < needleLength && needle.charAt(j) == haystack.charAt(i + j)) {
+        //         j++;
+        //     }
 
-            if (j == needleLength) return i;
-        }
+        //     if (j == needleLength) return i;
+        // }
 
-        return -1;
+        // return -1;
+
+        int index = (haystack.contains(needle)) ? haystack.indexOf(needle) : -1;
+        return index;
     }
 
     public static void main(String[] args) {
